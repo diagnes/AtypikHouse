8c29e375d2b230bdc30c4293fbdb645964eca5f8
Diagne Stephane <diagne_s@etna-alternance.net>
2018-07-27 02:18:28 +0200

    last modification ajout d'un petit system de notification

--- app/script/prod-debug.sh
+++ app/script/prod-debug.sh
@@ -4,9 +4,6 @@
 php bin/console assets:install;
 php bin/console assetic:dump;
 
-## Generation de la doc
-sudo php phpDocumentor.phar run -d src -t web/docs/
-
 ## Supression du cache de prod
 php bin/console cache:clear --env=prod;
 
